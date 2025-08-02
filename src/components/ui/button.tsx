478073618957d04892e352fd99f01a92b0b4f9e5
alihamasdev <alihamasdev@gmail.com>
2025-08-03 02:15:03 +0500

    add example env's, remove font-awesome icons and add lucide icons

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -22,7 +22,7 @@ export default function Button({
 				disabled={disabled}
 				whileTap={{ scale: 0.9 }}
 				className={cn(
-					"inline-flex items-center justify-center gap-2 rounded-lg text-sm font-medium text-zinc-300 transition-colors hover:bg-zinc-800/80 hover:text-zinc-50 disabled:opacity-50",
+					"inline-flex items-center justify-center gap-2 rounded-lg fill-zinc-300 text-sm font-medium text-zinc-300 transition-colors hover:bg-zinc-800/80 hover:fill-zinc-50 hover:text-zinc-50 disabled:opacity-50",
 					variant === "ghost" ? "bg-transparent" : "bg-zinc-800",
 					size === "icon" ? "h-10 w-10" : "h-10 px-4 py-2",
 					className
